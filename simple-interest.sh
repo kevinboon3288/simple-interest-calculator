9657d25a42495d8328db0ca44aa0e84b9f6df943
Kevin Boon <kevintan7227@gmail.com>
2025-06-01 18:07:54 +0800

    fix the error throw with bc

--- simple-interest.sh
+++ simple-interest.sh
@@ -12,5 +12,6 @@ read rate
 echo "Enter Time (in years):"
 read time
 
-si=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
-echo "Simple Interest is: $si"
+interest=$(( principal * rate * time / 100 ))
+
+echo "Simple Interest is: $interest"
